a39738773aacd6bdb043b56fc0f038a8903c6edd
Andrey Khapov <32270311+an-kh1409@users.noreply.github.com>
2017-12-26 13:19:29 +0300

    Add files via upload

---
+++ reflection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace lab6
+{
+    /// <summary>
+    /// Класс атрибута
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false,
+   Inherited = false)]
+    public class NewAttribute : Attribute
+    {
+        public NewAttribute() { }
+        public NewAttribute(string DescriptionParam)
+        {
+            Description = DescriptionParam;
+        }
+        public string Description { get; set; }
+    }
+}
